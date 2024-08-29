cde8e641ac9ce24a2d3ff6790eb9799505f1df21
ethanprogramser <168668562+ethanprogramser@users.noreply.github.com>
2024-08-29 17:29:17 +0000

    Add files via upload

--- c.c
+++ c.c
@@ -4,39 +4,45 @@
 #include<unistd.h>
 
 const gchar *ch;
+GtkWidget *window;
+GtkWidget *todo_label;
+GtkWidget *box;
+GtkWidget *create_new_button;
+GtkWidget *new;
+GtkWidget *save;
+GtkWidget *hardcode;
 
-static void delete(GtkWidget *del, gpointer delt) {
-	gchar *c = ch;	
-	gtk_button_set_label(GTK_BUTTON(del), c);	
-
-	gtk_widget_show_all(delt);
-
+static void delete(GtkWidget *p, gpointer data) {
+	gtk_widget_destroy(p);
 }
 
-static void save_(GtkWidget *ent, gpointer data, gpointer dat) {
-	ch = gtk_entry_get_text(GTK_ENTRY(data));
+static void save_(GtkWidget *ent, gpointer data) {
+	GtkWidget *new_but;
+
+	ch = gtk_entry_get_text(GTK_ENTRY(new));
 	
-	gtk_widget_destroy(dat);
-	gtk_widget_destroy(ent);
+	new_but = gtk_button_new_with_label(ch);
+
+	g_signal_connect(new_but, "clicked", G_CALLBACK(delete), NULL);
+	
+	gtk_container_add(GTK_CONTAINER(box), new_but);
+
+	gtk_widget_destroy(save);
+	gtk_widget_destroy(new);
+
 
 }
 
 static void read_file(GtkWidget *button, gpointer data) 
 {
-	GtkWidget *new;
-	GtkWidget *save;
-	GtkWidget *hard_code;
 	
-	hard_code = gtk_button_new_with_label("finalize");
 	save = gtk_button_new_with_label("Save");
 	new = gtk_entry_new();
 
 	g_signal_connect(save, "clicked", G_CALLBACK(save_),  new);
-	g_signal_connect(hard_code, "clicked", G_CALLBACK(delete), data);
 	
-	gtk_container_add(GTK_CONTAINER(data), new);
-	gtk_container_add(GTK_CONTAINER(data), save);
-	gtk_container_add(GTK_CONTAINER(data), hard_code);
+	gtk_container_add(GTK_CONTAINER(box), new);
+	gtk_container_add(GTK_CONTAINER(box), save);
 	
 	gtk_widget_show_all(data);
 }
@@ -44,11 +50,6 @@ static void read_file(GtkWidget *button, gpointer data)
 int main(int argc, char* argv[]) {
 
 	gtk_init(&argc, &argv);
-
-	GtkWidget *window;
-	GtkWidget *todo_label;
-	GtkWidget *box;
-	GtkWidget *create_new_button;
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	todo_label = gtk_label_new_with_mnemonic("_TODO");
